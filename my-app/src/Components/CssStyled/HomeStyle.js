25553c199a3a2ce1655d9cb7c488910537feddfd
Theja <gaddamtheja@gmail.com>
2024-04-16 23:13:11 +0530

    updated

---
+++ my-app/src/Components/CssStyled/HomeStyle.js
@@ -0,0 +1,15 @@
+import style from "styled-components"
+
+ export const Title = style.h1`
+ont-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`
+export const InputField = style.input`
+backgroundColor:"red"
+`
+export const Title1 = style.h2`
+ont-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`
--- my-app/src/Components/Home.jsx
+++ my-app/src/Components/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import {Title,InputField,Title1} from '../Components/CssStyled/HomeStyle'
 const Home = () => {
   return (
     <div>
       Home page
+      <Title>
+        Online shopping app
+      </Title>
+      <InputField>
+      </InputField>
+      <Title1>
+        Shop by Theja
+      </Title1>
     </div>
   )
 }
